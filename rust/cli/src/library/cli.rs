1207b00b6babe98b1b3b34529b71a2fb06a1669f
Vladimir Fomin <vladimir@fomin.xyz>
2025-03-13 00:49:39 +0100

    Add config files and refactor

--- rust/cli/src/library/cli.rs
+++ rust/cli/src/library/cli.rs
@@ -1,4 +1,7 @@
+use bollard::Docker;
 use clap::{Parser, Subcommand};
+
+use crate::library::docker::{setup_snapshot_volume, snapshot, verify_source_volume};
 
 /// A CLI tool for managing Docker volume snapshots.
 #[derive(Parser, Debug)]
@@ -19,7 +22,7 @@ pub enum Commands {
         compress: bool,
 
         /// Name of the volume to snapshot.
-        volume_name: String,
+        source_volume_name: String,
 
         /// Name of the snapshot.
         snapshot_name: String,
@@ -36,4 +39,57 @@ pub enum Commands {
         /// Name of the snapshot to delete.
         snapshot_name: String,
     },
+}
+
+pub async fn run() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Create {
+            source_volume_name,
+            compress,
+            snapshot_name,
+        } => create(source_volume_name, snapshot_name, compress).await?,
+        Commands::List => {
+            println!("Listing all snapshots...");
+            // TODO: Implement snapshot listing logic.
+        }
+        Commands::Restore { snapshot_name } => {
+            println!("Restoring volume from snapshot: {}", snapshot_name);
+            // TODO: Implement snapshot restoration logic.
+        }
+        Commands::Delete { snapshot_name } => {
+            println!("Deleting snapshot: {}", snapshot_name);
+            // TODO: Implement snapshot deletion logic.
+        }
+    }
+
+    Ok(())
+}
+
+async fn create(
+    source_volume_name: String,
+    snapshot_name: String,
+    compress: bool,
+) -> anyhow::Result<()> {
+    let docker = Docker::connect_with_local_defaults()?;
+
+    let snapshot_volume_name =
+        get_snapshot_volume_name(chrono::Utc::now().timestamp(), &snapshot_name);
+
+    verify_source_volume(&docker, &source_volume_name).await?;
+    setup_snapshot_volume(&docker, &snapshot_volume_name).await?;
+    snapshot(
+        &docker,
+        &source_volume_name,
+        &snapshot_volume_name,
+        compress,
+    )
+    .await?;
+
+    Ok(())
+}
+
+fn get_snapshot_volume_name(timestamp: i64, name: &str) -> String {
+    format!("vsnap-{}-{}", timestamp, name)
 }
---
+++ rust/cli/src/library/docker.rs
@@ -0,0 +1,122 @@
+use anyhow::anyhow;
+use bollard::{
+    Docker,
+    container::{Config, CreateContainerOptions, StartContainerOptions, WaitContainerOptions},
+    secret::{HostConfig, Mount},
+    volume::CreateVolumeOptions,
+};
+use futures::StreamExt;
+
+pub async fn verify_source_volume(docker: &Docker, source_volume_name: &str) -> anyhow::Result<()> {
+    // Check if the volume exists.
+    let volume = match docker.inspect_volume(&source_volume_name).await.ok() {
+        Some(volume) => volume,
+        None => {
+            return Err(anyhow!("Volume {} does not exist.", source_volume_name));
+        }
+    };
+
+    // Check if the volume is in use.
+    if let Some(usage) = volume.usage_data {
+        if usage.ref_count > 0 {
+            return Err(anyhow!("Volume {} is in use.", source_volume_name));
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn setup_snapshot_volume(
+    docker: &Docker,
+    snapshot_volume_name: &str,
+) -> anyhow::Result<()> {
+    // Create a new volume for the snapshot.
+    let config = CreateVolumeOptions {
+        name: snapshot_volume_name,
+        ..Default::default()
+    };
+
+    docker.create_volume(config).await?;
+
+    Ok(())
+}
+
+pub async fn snapshot(
+    docker: &Docker,
+    source_volume_name: &str,
+    snapshot_volume_name: &str,
+    compress: bool,
+) -> anyhow::Result<()> {
+    const SOURCE_DIR: &str = "/mnt/source";
+    const SNAPSHOT_DIR: &str = "/mnt/snapshot";
+
+    let container_name = format!("vsnap-{}", chrono::Utc::now().timestamp());
+
+    // TODO: Make sure the container is removed at the end.
+    let options = Some(CreateContainerOptions {
+        name: container_name.clone(),
+        platform: None,
+    });
+
+    let mut cmd = vec!["snapshot"];
+
+    if compress {
+        cmd.push("--compress");
+    }
+
+    cmd.extend(vec![SOURCE_DIR, SNAPSHOT_DIR]);
+
+    let host_config = HostConfig {
+        mounts: Some(vec![
+            Mount {
+                source: Some(source_volume_name.to_string()),
+                target: Some(SOURCE_DIR.to_string()),
+                typ: Some(bollard::secret::MountTypeEnum::VOLUME),
+                read_only: Some(true),
+                ..Default::default()
+            },
+            Mount {
+                source: Some(snapshot_volume_name.to_string()),
+                target: Some(SNAPSHOT_DIR.to_string()),
+                typ: Some(bollard::secret::MountTypeEnum::VOLUME),
+                ..Default::default()
+            },
+        ]),
+        ..Default::default()
+    };
+
+    // TODO: Fix the version in the binary
+    let config = Config {
+        image: Some("vsnap:latest"),
+        cmd: Some(cmd),
+        host_config: Some(host_config),
+        // TODO: Add tty support
+        // tty: Some(true),
+        // open_stdin: Some(true),
+        // stdin_once: Some(true),
+        ..Default::default()
+    };
+
+    match (async || {
+        docker.create_container(options, config).await?;
+
+        docker
+            .start_container(&container_name, None::<StartContainerOptions<String>>)
+            .await?;
+
+        docker
+            .wait_container(&container_name, None::<WaitContainerOptions<String>>)
+            .for_each(async |_| {})
+            .await;
+
+        Ok::<(), anyhow::Error>(())
+    })()
+    .await
+    {
+        result => {
+            docker.remove_container(&container_name, None).await?;
+
+            result
+        }
+    }
+}
