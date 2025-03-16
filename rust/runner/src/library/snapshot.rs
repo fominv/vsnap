adeba1ceba2de45bb6997fdcbd0815312ac9902c
Vladimir Fomin <vladimir@fomin.xyz>
2025-03-17 00:52:51 +0100

    Reset unnecessary change

--- rust/runner/src/library/snapshot.rs
+++ rust/runner/src/library/snapshot.rs
@@ -5,14 +5,12 @@ use async_compression::{
     Level,
     tokio::{bufread::ZstdDecoder, write::ZstdEncoder},
 };
-use futures_util::{StreamExt, TryStreamExt};
 use tokio::{
     fs::File,
     io::BufStream,
     sync::mpsc::{self, Sender},
 };
 use tokio_tar::Builder;
-use walkdir::WalkDir;
 
 use crate::library::{
     constant::{SNAPSHOT_METADATA, SNAPSHOT_TAR, SNAPSHOT_TAR_ZST},
@@ -91,43 +89,10 @@ async fn compress_dir(
 ) -> anyhow::Result<()> {
     let buffered_stream = BufStream::new(tar_file);
     let encoder = ZstdEncoder::with_quality(buffered_stream, Level::Default);
-    // let progress_writer = AsyncProgressReaderWriter::new(encoder, sender);
+    let progress_writer = AsyncProgressReaderWriter::new(encoder, sender);
 
-    let mut archive = Builder::new(encoder);
-    // archive.append_dir_all(".", dir_to_compress).await?;
-    // archive.finish().await?;
-
-    for entry in WalkDir::new(&dir_to_compress)
-        .follow_links(false)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        let relative_path = entry.path().strip_prefix(&dir_to_compress)?;
-
-        if relative_path == Path::new("") {
-            continue;
-        }
-
-        let file_type = entry.file_type();
-
-        if file_type.is_symlink() {
-            // let target = std::fs::read_link(entry.path())?;
-
-            // let mut header = Header::new_gnu();
-            // header.set_entry_type(EntryType::Symlink);
-            // header.set_size(0);
-
-            // tar_builder.append_link(&mut header, relative_path, &target)?;
-
-            continue;
-        } else if file_type.is_dir() {
-            archive.append_dir(relative_path, entry.path()).await?;
-        } else if file_type.is_file() {
-            let mut file = File::open(entry.path()).await?;
-            archive.append_file(relative_path, &mut file).await?;
-        }
-    }
-
+    let mut archive = Builder::new(progress_writer);
+    archive.append_dir_all(".", dir_to_compress).await?;
     archive.finish().await?;
 
     Ok(())
@@ -141,32 +106,18 @@ async fn decompress_dir(
     let buffered_stream = BufStream::new(tar_file);
     let decoder = ZstdDecoder::new(buffered_stream);
 
-    // let progress_writer = AsyncProgressReaderWriter::new(decoder, sender);
-    let mut archive = tokio_tar::Archive::new(decoder);
-
-    println!("Restoring to {:?}", restore_path);
-
-    archive
-        .entries()?
-        .try_for_each_concurrent(10, |entry| async {
-            let mut entry = entry;
-            let path = restore_path.join(entry.path()?);
-            entry.unpack(&path).await?;
-
-            Ok(())
-        })
-        .await?;
-
-    println!("Restored to {:?}", restore_path);
+    let progress_writer = AsyncProgressReaderWriter::new(decoder, sender);
+    let mut archive = tokio_tar::Archive::new(progress_writer);
+    archive.unpack(restore_path).await?;
 
     Ok(())
 }
 
 async fn tar_dir(dir: &Path, tar_file: tokio::fs::File, sender: Sender<u64>) -> anyhow::Result<()> {
     let buffered_stream = BufStream::new(tar_file);
-    // let progress_writer = AsyncProgressReaderWriter::new(buffered_stream, sender);
+    let progress_writer = AsyncProgressReaderWriter::new(buffered_stream, sender);
 
-    let mut archive = Builder::new(buffered_stream);
+    let mut archive = Builder::new(progress_writer);
 
     archive.append_dir_all(".", dir).await?;
     archive.finish().await?;
@@ -180,26 +131,10 @@ async fn untar_dir(
     sender: Sender<u64>,
 ) -> anyhow::Result<()> {
     let buffered_stream = BufStream::new(tar_file);
-    let decoder = ZstdDecoder::new(buffered_stream);
+    let progress_writer = AsyncProgressReaderWriter::new(buffered_stream, sender);
 
-    // let progress_writer = AsyncProgressReaderWriter::new(decoder, sender);
-
-    let mut archive = tokio_tar::Archive::new(decoder);
-
-    println!("Restoring to {:?}", restore_path);
-
-    archive
-        .entries()?
-        .try_for_each_concurrent(10, |entry| async {
-            let mut entry = entry;
-            let path = restore_path.join(entry.path()?);
-            entry.unpack(&path).await?;
-
-            Ok(())
-        })
-        .await?;
-
-    println!("Restored to {:?}", restore_path);
+    let mut archive = tokio_tar::Archive::new(progress_writer);
+    archive.unpack(restore_path).await?;
 
     Ok(())
 }
